d26ed3a9232ebc5718ae0021d93904bfca3b1587
Andrzej Legutko <andrzejlegutko@gmail.com>
2024-04-15 07:25:28 +0200

    add others component

---
+++ src/OtherWebRtcComponent.jsx
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+
+function OtherWebRtcComponent({othersIds, peer, index, mvOtherId}) {
+    const [connection, setConnection] = useState(null);
+    const [isConnecting, setIsConnecting] = useState(false);
+
+    useEffect(() => {
+        if (!isConnecting) {
+            return;
+        }
+        const conn = peer.connect(`lurco_${othersIds[index]}`);
+        conn.on('open', () => {
+            conn.send(`Connected to ${othersIds[index]}!`);
+        });
+        setConnection(conn);
+
+        return () => {
+            conn.close();
+        }
+    }, [isConnecting]);
+
+
+
+    return (
+        <form>
+            <input type="text" value={othersIds[index]} onChange={(e) => {
+                mvOtherId(index, e.target.value)
+            }}/>
+            <button onClick={() => {
+                setIsConnecting(true);
+            }}>Utwórz połączenie
+            </button>
+        </form>
+    );
+}
+
+export default OtherWebRtcComponent;
--- src/WebRtcComponent2.jsx
+++ src/WebRtcComponent2.jsx
@@ -1,17 +1,40 @@
 import Peer from "peerjs";
+import {useCallback, useEffect, useState} from "react";
+import OtherWebRtcComponent from "./OtherWebRtcComponent.jsx";
 
 function WebRtcComponent2() {
+    const [myId, setMyId] = useState('');
+    const [otherIds, setOthersIds] = useState([]);
+    const mvOtherId = useCallback((index, value) => {
+        setOthersIds((othersIds) => {
+            const newOthersIds = [...othersIds];
+            newOthersIds[index] = value;
+            return newOthersIds;
+        });
+    }, []);
+    let peer = null;
 
-    const openConnection = () => {
-        const peer = new Peer('this_unique_id_is_very_unique');
+    const openMyConnection = () => {
+        peer = new Peer(`lurco_${myId}`);
         peer.on('open', (id) => {
             console.log('My peer ID is: ' + id);
         });
     }
 
+    const addConnection = () => {
+        mvOtherId(otherIds.length, '');
+    }
+
     return (
         <div>
-            <button onClick={openConnection}>Kliknij mnie!</button>
+            <form>
+                <input type="text" value={myId} onChange={(e) => setMyId(e.target.value)}/>
+                <button onClick={openMyConnection}>Kliknij mnie!</button>
+            </form>
+            {/*{otherIds.map((id, index) => (*/}
+            {/*    <OtherWebRtcComponent othersIds={otherIds} mvOtherId={mvOtherId} peer={peer} index={index} key={otherIds[index]}/>*/}
+            {/*))}*/}
+            {/*<button onClick={addConnection}>Dodaj połączenie</button>*/}
         </div>
     );
 }
